323c0c64ff966a165c080f3e4988dc615b7fa178
Hector Miranda <marcetux@gmail.com>
2016-12-04 21:12:05 +0000

    Added some super basic filtering

--- app/controllers/profile/pets_controller.rb
+++ app/controllers/profile/pets_controller.rb
@@ -2,14 +2,17 @@ module Profile
   class PetsController < ProfileController
 
     def index
-      @pets = Pet.most_recent.paginate(:page => params[:page], per_page: 3)
-
+      @pets = Pet.most_recent
+      @pets = @pets.breed(params[:breed]) if params[:breed].present?
+      @pets = @pets.sex(params[:sex]) if params[:sex].present?
+      @pets = @pets.age(params[:age]) if params[:age].present?
+      @pets = @pets.paginate(:page => params[:page], per_page: 3)
     end
 
     def show
       @pet = Pet.friendly.find(params[:id])
       @comments = Comment.where(pet_id:  @pet)
-
     end
+
   end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,13 +4,17 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_many :comments
   scope :most_recent, -> { order(id: :desc) }
-
+  scope :breed, -> (breed){ where("breed LIKE ?", "%#{breed}%")}
+  scope :sex, -> (sex){ where("sex = ?", "#{sex}")}
+  scope :age, -> (age){ where("age = ?", "#{age}")}
 
   def should_generate_new_friendly_id?
     name_changed?
   end
 
   def owner
-    "Owner"
+   #User.find_by(id: user_id).name 
   end
+
+
 end
